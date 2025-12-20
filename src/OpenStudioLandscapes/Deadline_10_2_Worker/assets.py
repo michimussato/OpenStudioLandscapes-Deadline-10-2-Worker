2a292e9bc963c124d313d54d95e59ec3b2a94a7c
Michael Mussato <michimussato@gmail.com>
2025-12-21 00:54:31 +0100

    47-refactor-to-config-based-landscapes: OpenStudioLandscapes-OpenCue and OpenStudioLandscapes-OpenCue-Worker

--- src/OpenStudioLandscapes/Deadline_10_2_Worker/assets.py
+++ src/OpenStudioLandscapes/Deadline_10_2_Worker/assets.py
@@ -54,6 +54,8 @@ yaml.SafeDumper.add_multi_representer(
 )
 
 
+# Todo
+#  - [ ] a la OpenCue with intermediate compose_override
 # Overridden locally
 # compose_scope_group__cmd: AssetsDefinition = get_compose_scope_group__cmd(
 #     ASSET_HEADER=ASSET_HEADER,
