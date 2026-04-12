16d9cbdf34e026d3db4c11a78e4d626a09b84b5c
Michael Mussato <michimussato@gmail.com>
2026-04-12 14:10:05 +0200

    all Features should now all work individually with `dagster dev --workspace workspace.yaml`.

--- src/OpenStudioLandscapes/Deadline_10_2_Worker/_definitions_with_upstream_specs.py
+++ src/OpenStudioLandscapes/Deadline_10_2_Worker/_definitions_with_upstream_specs.py
@@ -1,26 +1,20 @@
 from dagster import Definitions
 from OpenStudioLandscapes.Deadline_10_2.assets import (
-    build_docker_image_client_spec,
-    # compose_pulse_runner_spec,
-    # compose_pulse_runner,
-    deadline_command_compose_pulse_runner_spec,
-    deadline_command_compose_worker_runner_spec,
+    build_docker_image_client,
+    deadline_command_compose_pulse_runner,
+    deadline_command_compose_worker_runner,
     feature_out_v2,
 )
-from OpenStudioLandscapes.engine.base.assets import group_out_base_spec
+from OpenStudioLandscapes.engine.base.assets import group_out_base
 
 from OpenStudioLandscapes.Deadline_10_2_Worker.definitions import assets_base
 
 
-# from OpenStudioLandscapes.Deadline_10_2.assets import feature_out_v2
-
 assets_external = []
-assets_external.append(group_out_base_spec)
-# assets_external.append(compose_pulse_runner_spec)
-# assets_external.extend(compose_pulse_runner.specs)
-assets_external.append(deadline_command_compose_worker_runner_spec)
-assets_external.append(deadline_command_compose_pulse_runner_spec)
-assets_external.append(build_docker_image_client_spec)
+assets_external.extend(group_out_base.specs)
+assets_external.extend(deadline_command_compose_worker_runner.specs)
+assets_external.extend(deadline_command_compose_pulse_runner.specs)
+assets_external.extend(build_docker_image_client.specs)
 assets_external.extend(feature_out_v2.specs)
 
 
