b56529ec770ca6f03a6b07dc628271f3c1398f1d
Michael Mussato <michimussato@gmail.com>
2026-05-06 15:14:08 +0200

    nox

--- src/OpenStudioLandscapes/Deadline_10_2_Worker/config/models.py
+++ src/OpenStudioLandscapes/Deadline_10_2_Worker/config/models.py
@@ -7,7 +7,7 @@ from pydantic import (
     PositiveInt,
 )
 
-from OpenStudioLandscapes.Deadline_10_2_Worker import constants, dist, LOGGER
+from OpenStudioLandscapes.Deadline_10_2_Worker import LOGGER, constants, dist
 
 
 class Config(FeatureBaseModel):
