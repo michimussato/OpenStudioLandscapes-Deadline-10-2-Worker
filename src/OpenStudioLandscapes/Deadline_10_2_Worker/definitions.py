65e5763e85e3b781b508a3231f50e629b313f6e6
Michael Mussato <michimussato@gmail.com>
2026-08-20 14:55:13 +0200

    fix updated imports

--- src/OpenStudioLandscapes/Deadline_10_2_Worker/definitions.py
+++ src/OpenStudioLandscapes/Deadline_10_2_Worker/definitions.py
@@ -4,7 +4,7 @@ from dagster import (
 )
 
 import OpenStudioLandscapes.Deadline_10_2_Worker.assets
-from OpenStudioLandscapes.Deadline_10_2_Worker import (
+from OpenStudioLandscapes.Deadline_10_2_Worker.constants import (
     LOGGER,
     dist,
 )
